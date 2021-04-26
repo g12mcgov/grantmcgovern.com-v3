805c3574d59c202f6a8cef180070d0dff3e922a9
Grant McGovern <grant@cobrain.com>
2021-04-26 18:52:38 -0400

    Add cert check

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  get '/.well-known/acme-challenge/ueQEKzpHfECpveKU6GGWEYsAz6cULbNrt6YCTYdNGWQ', :to => redirect('/ueQEKzpHfECpveKU6GGWEYsAz6cULbNrt6YCTYdNGWQ')
 end
