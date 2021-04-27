c443f8d959f17217e750f7d78b8b768b02f44f72
Grant McGovern <grant@cobrain.com>
2021-04-26 20:04:18 -0400

    Remove SSL check files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  get '/.well-known/acme-challenge/ueQEKzpHfECpveKU6GGWEYsAz6cULbNrt6YCTYdNGWQ', :to => redirect('/ueQEKzpHfECpveKU6GGWEYsAz6cULbNrt6YCTYdNGWQ')
 end
